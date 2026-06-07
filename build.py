920453b5df6306dd659ed5cec15d332960eb8944
Marcell Nagy <marcelldnagy@gmail.com>
2026-06-07 16:17:20 +0200

    Start building

---
+++ build.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from shutil import copy2, rmtree
+
+DEPS = Path(__file__).parent / "modules"
+TARGET = Path(__file__).parent / "dist"
+TEMPLATES = Path(__file__).parent / "templates"
+
+
+def fetch_and_build():
+
+    if TARGET.exists():
+        rmtree(TARGET)
+    TARGET.mkdir()
+    copy2(DEPS / "index.global.js", TARGET / "index.global.js")
+    copy2(TEMPLATES / "template.html", TARGET / "index.global.js")
+
+
+if __name__ == "__main__":
+    fetch_and_build()
